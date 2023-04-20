eb1f6a25046850a3d3f4aa01aeff53f686dee9d0
Vincent Haron C. Mamutuk <vhcblade@gmail.com>
2023-04-21 00:35:10 +0800

    Added Errors

--- lib/bookmark_requests.dart
+++ lib/bookmark_requests.dart
@@ -3,3 +3,4 @@ library bookmark_requests;
 export 'bookmark_models.dart';
 export 'src/requests/share_request.dart';
 export 'src/requests/sync.dart';
+export 'src/requests/errors.dart';
---
+++ lib/src/requests/errors.dart
@@ -0,0 +1,8 @@
+enum BookmarkErrorStatusCodes {
+  error460("Bookmark Already Exists"),
+  ;
+
+  final String error;
+
+  const BookmarkErrorStatusCodes(this.error);
+}
